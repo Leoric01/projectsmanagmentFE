71999948f2a90bea57bd9eee1453849829750b47
Leoric01 <troglodit.nick@seznam.cz>
2025-05-26 22:37:47 +0200

    again naming and imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "@/Redux/Store";
+import { store } from "src/Redux/Store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
