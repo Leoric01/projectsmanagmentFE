3fdbd81496da2837dff54c1b48bfa84fa57dcc38
Leoric01 <troglodit.nick@seznam.cz>
2025-05-26 21:46:52 +0200

    upgraded base url

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,34 +1,38 @@
 import axios from "axios";
 
 // Set base URL for your API
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://projectsmanagment-production.up.railway.app";
 
 // Create axios instance
 const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    }
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem("jwt");
     if (token) {
-        config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
-}, (error) => {
+  },
+  (error) => {
     return Promise.reject(error);
-});
+  }
+);
 
 api.interceptors.response.use(
-    (response) => response, (error) => {
-        if (error.response?.status === 401) {
-            console.log("Unauthorized, logging out...");
-            localStorage.removeItem("jwt");
-        }
-        return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log("Unauthorized, logging out...");
+      localStorage.removeItem("jwt");
     }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
